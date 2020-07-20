37908c1cd8de53a021f9d95c2a970b90cea7188f
estevaog3 <estevao.costa.dev@gmail.com>
2020-07-20 11:57:23 -0400

    refactor: split scraping process based on NODE_ENV

--- scraper/src/main.js
+++ scraper/src/main.js
@@ -3,10 +3,14 @@ const fs = require('fs');
 const Enrollment = require('./Page/Enrollment.js');
 const searchClient = require('../../services/searchClient');
 
-(async () => {
+async function scrapeAndSave() {
   const username = process.env.USUARIO;
   const password = process.env.SENHA;
-  const outputFile = process.env.OUTPUT_FILE;
+  const outputFile = process.env.PROD_OUTPUT_FILE;
+  const index = process.env.PROD_INDEX;
+  if (!username || !password || !outputFile || !index) {
+    throw Error('username, password, outputFile or index is undefined');
+  }
   let courses;
   try {
     await Enrollment.signIn(username, password);
@@ -16,6 +20,24 @@ const searchClient = require('../../services/searchClient');
     console.error('ERROR!\n', e);
   }
   fs.writeJson(outputFile, courses);
-  searchClient.indexAll(courses);
+  searchClient.indexAll(courses, index);
   await Enrollment.close();
+}
+
+function loadAndSave() {
+  const index = process.env.DEV_INDEX;
+  const inputFile = process.env.DEV_INPUT_FILE;
+  const courses = require(inputFile.toString());
+  if (!index || !inputFile || !courses) {
+    throw Error('index, inputFile or courses is undefined');
+  }
+  searchClient.indexAll(courses, index);
+}
+
+(() => {
+  if (process.env.NODE_ENV === 'production') {
+    scrapeAndSave();
+  } else {
+    loadAndSave();
+  }
 })();
