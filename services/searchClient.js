48df06db5a61078c4d5998254b584fab92bdb564
estevaog3 <estevao.costa.dev@gmail.com>
2020-07-20 11:54:19 -0400

    feat(searchClient): add index parameter on indexAll method

--- services/searchClient.js
+++ services/searchClient.js
@@ -2,13 +2,12 @@ const { Client } = require('@elastic/elasticsearch');
 
 const client = new Client({ node: 'http://localhost:9200' });
 
-exports.indexAll = async function indexAll(records) {
-  console.log(process.env.INDEX);
+exports.indexAll = async function indexAll(records, index) {
   try {
     await Promise.all(
       records.map((record) =>
         client.index({
-          index: 'turmas', // TODO: remove this hardcoded value
+          index,
           body: record,
         }),
       ),
