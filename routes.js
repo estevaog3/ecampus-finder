daaafd368e6aaa8259ff8b8f9825757f984ad343
estevaog3 <estevao.costa.dev@gmail.com>
2020-07-21 16:58:26 -0400

    fix: update search's route HTTP method to match API definition

--- routes.js
+++ routes.js
@@ -2,8 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/search', (req, res, next) => {
+router.post('/search', (req, res, next) => {
   console.log('params:', req.query);
   res.json([{ res: 'Hello world' }]);
 });
