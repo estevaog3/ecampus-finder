893681dd773cb6ddb0e79d270ce1423f4ef13214
estevaog3 <estevao.costa.dev@gmail.com>
2020-07-23 13:53:51 -0400

    feat(router): use searchClient.query(..) in /search
    This commit adds tramsformQuery(...) to allow for AND and OR operations to work properly. Example of transformation: "segunda e terça ou quinta" -> "segunda AND terça OR quinta".

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
 module.exports = {
   BULK_TIMEOUT: '60000ms', // 60 seconds timeout
   SEARCH_TIMEOUT: '50000ms', // 5 seconds timeout
+  PROD_INDEX: 'turmas',
 };
--- routes.js
+++ routes.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const searchClient = require('./services/searchClient');
+const { PROD_INDEX } = require('./constants');
 
 const router = express.Router();
 
-router.post('/search', (req, res, next) => {
-  console.log('params:', req.query);
-  res.json([{ res: 'Hello world' }]);
+const transformQuery = (query) => {
+  return query
+    .replace(/( e )|( E )/g, ' AND ')
+    .replace(/( ou )|( OU )|( Ou )|( oU )/g, ' OR ');
+};
+
+router.post('/search', async (req, res) => {
+  const { query, limit, offset, sort } = req.query;
+  const { filter } = req.body;
+  const hits = await searchClient.query(
+    PROD_INDEX,
+    transformQuery(query),
+    limit,
+    offset,
+    sort,
+    filter,
+  );
+  if (!hits) {
+    res.status(500).send({ code: 500, message: 'Search failed' });
+    return;
+  }
+  res.json(hits);
 });
 
 module.exports = router;
