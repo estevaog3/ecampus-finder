4e2e58e971475ceb604f301bdeedaa04da04b89c
estevaog3 <estevao.costa.dev@gmail.com>
2020-09-01 17:46:26 -0400

    refactor(scraper): remove code related to dev environment
    This commits removes dev environment code. Since the project has changed to be run only locally, then there's no need yet to separate development and production environments.

--- scraper/src/main.js
+++ scraper/src/main.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
-const fs = require('fs');
 const Enrollment = require('./Page/Enrollment.js');
 const searchClient = require('../../services/searchClient');
+const { PROD_INDEX } = require('../../constants');
 
 async function scrapeAndSave() {
   const username = process.env.USUARIO;
   const password = process.env.SENHA;
-  const outputFile = process.env.PROD_OUTPUT_FILE;
-  const index = process.env.PROD_INDEX;
-  if (!username || !password || !outputFile || !index) {
-    throw Error('username, password, outputFile or index is undefined');
+
+  if (!username || !password) {
+    throw Error('username or password is undefined');
   }
   let courses;
   try {
@@ -19,27 +18,9 @@ async function scrapeAndSave() {
   } catch (e) {
     console.error('ERROR!\n', e);
   }
-  fs.writeJson(outputFile, courses);
-  searchClient.init(index);
-  searchClient.indexAll(courses, index);
+  searchClient.init(PROD_INDEX);
+  searchClient.indexAll(courses, PROD_INDEX);
   await Enrollment.close();
 }
 
-function loadAndSave() {
-  const index = process.env.DEV_INDEX;
-  const inputFile = process.env.DEV_INPUT_FILE;
-  const courses = require(inputFile.toString());
-  if (!index || !inputFile || !courses) {
-    throw Error('index, inputFile or courses is undefined');
-  }
-  searchClient.init(index);
-  searchClient.indexAll(courses, index);
-}
-
-(() => {
-  if (process.env.NODE_ENV === 'production') {
-    scrapeAndSave();
-  } else {
-    loadAndSave();
-  }
-})();
+scrapeAndSave();
