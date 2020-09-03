0648288444887fea4083442cdb919619eba4af21
estevaog3 <estevao.costa.dev@gmail.com>
2020-09-02 22:06:52 -0400

    feat: add Logo and SearchBar to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Logo from "./components/Logo/index";
+import SearchBar from "./components/SearchBar/index";
 
 function App() {
   return (
     <div>
-      <p>Hello world</p>
+      <Logo slogan="Encontre facilmente turmas da 2º fila de matrícula da UFAM" />
+      <SearchBar placeholder="disciplina, curso, horário..." />
     </div>
   );
 }
