0bda1dad4a25f7c09b8e47489845ae4e32838c25
estevaog3 <estevao.costa.dev@gmail.com>
2020-09-03 22:00:32 -0400

    feat(SearchBar): add search controls behavior and styles
    This commit also removes an unused prop in SearchBar component:
    classNames.

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,15 +1,51 @@
 import React from "react";
-
+import { useState } from "react";
 import "./styles.css";
+import { ReactComponent as SearchIcon } from "./search.svg";
+import { ReactComponent as CloseIcon } from "./close.svg";
 
-function SearchBar({ placeholder, className }) {
+function SearchBar({ placeholder }) {
+  const [query, setQuery] = useState("");
+
+  const onQueryChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const onCloseIconClick = () => {
+    setQuery("");
+  };
+
+  const onSearchIconClick = () => {
+    //TODO: consultar API de turmas
+    console.log("query:", query);
+  };
+
+  const toggleCloseIconClass = () => {
+    if (query.length == 0) {
+      return "";
+    }
+    return "search-control-close--appear-yes";
+  };
+
   return (
-    <form className={className}>
+    <form className="search-bar-wrap">
       <input
         className="search-bar-input"
         type="text"
         placeholder={placeholder}
+        value={query}
+        onChange={(e) => onQueryChange(e)}
       />
+      <div className="search-controls">
+        <CloseIcon
+          onClick={onCloseIconClick}
+          className={"search-control-close " + toggleCloseIconClass()}
+        />
+        <SearchIcon
+          onClick={onSearchIconClick}
+          className="search-control-search"
+        />
+      </div>
     </form>
   );
 }
