52a8ee5fd19975bfb66fc69d44043d80f2b704fd
estevaog3 <eccosta@protonmail.com>
2022-02-23 20:51:34 -0400

    feat(scraper): optionally index classes from classes file

--- backend/scraper/src/main.js
+++ backend/scraper/src/main.js
@@ -1,26 +1,47 @@
 require("dotenv").config();
 const Enrollment = require("./Page/Enrollment.js");
 const searchClient = require("../../services/searchClient");
+const fs = require('fs');
 const { PROD_INDEX } = require("../../constants");
 
-async function scrapeAndSave() {
+async function indexClasses(classes) {
+  await searchClient.init(PROD_INDEX);
+  await searchClient.indexAll(classes, PROD_INDEX);
+}
+
+async function scrapeClasses() {
   const username = process.env.USUARIO;
   const password = process.env.SENHA;
 
   if (!username || !password) {
     throw Error("username or password is undefined");
   }
-  let courses;
+  let classes;
   try {
     await Enrollment.signIn(username, password);
     console.log("signIn passed");
-    courses = await Enrollment.scrapeAllCourses();
+    classes = await Enrollment.scrapeAllCourses();
   } catch (e) {
     console.error("ERROR!\n", e);
   }
-  searchClient.init(PROD_INDEX);
-  searchClient.indexAll(courses, PROD_INDEX);
   await Enrollment.close();
+  return classes;
 }
 
-scrapeAndSave();
+async function main() {
+  let classesFile = process.argv[2]
+  if(classesFile){
+    fs.readFile(classesFile, (error, data) => {
+      if(error){
+        console.log(error);
+        return;
+      }
+      indexClasses(JSON.parse(data));
+    })
+  }else {
+    const classes = await scrapeClasses();
+    indexClasses(classes);
+  }
+}
+
+main();
