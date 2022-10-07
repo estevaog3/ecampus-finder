f835342dbaec2cc13ea0d5e15e980cae3cee63fe
estevaog3 <eccosta@protonmail.com>
2022-10-07 18:48:04 -0400

    feat(ResultsPage): add filter layout and styles

--- frontend/src/components/ResultsPage/index.js
+++ frontend/src/components/ResultsPage/index.js
@@ -7,7 +7,7 @@ import api from "../../services/api";
 import queryString from "query-string";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { MultiSelect } from "react-multi-select-component";
 import { fixedEncodeURIComponent } from "../../util/index";
 import "./styles.css";
 import Result from "../Result/index";
@@ -22,10 +22,20 @@ let searchParams = {
   limit: 10,
 };
 
+const SEARCH_DAYS = [
+  { label: "Seg", value: "segunda" },
+  { label: "Ter", value: "terça" },
+  { label: "Qua", value: "quarta" },
+  { label: "Qui", value: "quinta" },
+  { label: "Sex", value: "sexta" },
+  { label: "Sáb", value: "sábado" },
+];
+
 function ResultsPage({ history, location }) {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [hasMoreResults, setHasMoreResults] = useState(undefined);
+  const [searchDays, setSearchDays] = useState(SEARCH_DAYS);
 
   const encodeQuery = (query) => {
     return fixedEncodeURIComponent(queryString.parse(query).query);
@@ -139,6 +149,44 @@ function ResultsPage({ history, location }) {
           initialQuery={queryString.parse(location.search).query}
         />
       </header>
+      <div className="filter">
+        <div className="filter__days">
+          <div>Dias da semana:</div>
+          <MultiSelect
+            options={SEARCH_DAYS}
+            value={searchDays}
+            onChange={setSearchDays}
+            labelledBy="Selecione"
+            disableSearch
+            overrideStrings={{
+              allItemsAreSelected: "Qualquer dia",
+              clearSelected: "Clear Selected",
+              selectAll: "Selecionar todos",
+              selectSomeItems: "Selecione...",
+            }}
+          />
+        </div>
+        <div className="filter__time">
+          <div>
+            <label htmlFor="startTime">De</label>
+            <input
+              type="time"
+              name="startTime"
+              id="startTime"
+              onChange={(e) => console.log(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="endTime">até</label>
+            <input
+              type="time"
+              name="endTime"
+              id="endTime"
+              onChange={(e) => console.log(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
       <div className="body">
         {renderResults(results)}
         {results.length === 0 ? (
